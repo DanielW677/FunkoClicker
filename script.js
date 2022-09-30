f539699e67e36c7c251641ebee550edc3693c707
ddawg776 <112032441+ddawg776@users.noreply.github.com>
2022-09-30 10:47:32 -0500

    Added clicking to image to add too score

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let currentFunkoCollected = document.getElementById("FunkoCounter")
-let currentGeneratedFunko = document.getElementById("GeneratedFunko")
+let score = 0;
+let scoreGeneration = 0;
+const button = document.getElementById('ClickMe');
+const scoreDisplay = document.getElementById('FunkoCounter');
+const commonButton = document.getElementById('CommonFunko');
+const glitterButton = document.getElementById('GlitterFunko');
+const blackLightButton = document.getElementById('BlacklightFunko');
+const chaseButton = document.getElementById('ChaseFunko');
+const freddyButton = document.getElementById('freddyFunko');
+
+
+button.addEventListener('click', function(clickEvent){
+    button.click();
+    score++;
+    scoreDisplay.innerText = score;
+})
